8484f4c45ee114c6b91e3034527e54eadc9b6912
Blankeos <carloantonioct@gmail.com>
2024-02-20 00:01:10 +0800

    feat: HUGE UI revamp and less bugs.

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,17 +1,22 @@
-import { createSignal, For, onMount, Show } from "solid-js";
+import { createSignal, For, Show } from "solid-js";
+import DAYJS_typedefs from "dayjs/index.d.ts?raw";
 import { MonacoEditor } from "solid-monaco";
 import { Panel, PanelGroup, ResizeHandle } from "solid-resizable-panels";
 
-import Counter from "@/components/Counter";
-import DAYJS_index from "@/constants/dayjs.txt?raw";
+import { A } from "@solidjs/router";
+
+import IconExecute from "~icons/material-symbols-light/play-arrow-outline";
 
 import "solid-resizable-panels/styles.css";
 
-export default function Home() {
-  const [tsCodeState, setTSCodeState] = createSignal(
-    `console.log('Hello World');\nimport dayjs from "dayjs"\nimport {add} from 'math';\nconst x = add(3, 5);\n`,
-  );
+const DAYJS_index_replaced = DAYJS_typedefs.replace(
+  "export = dayjs;",
+  "declare global dayjs",
+);
 
+export default function Home() {
+  const [showAdvanced, setShowAdvanced] = createSignal(false);
+  const [tsCodeState, setTSCodeState] = createSignal("");
   const [jsCodeState, setJSCodeState] = createSignal("");
   const [codeLogs, setCodeLogs] = createSignal<string[]>();
 
@@ -22,7 +27,7 @@ export default function Home() {
     const originalLog = console.log;
 
     console.log = function (...args) {
-      logs.push(args.join(" "));
+      logs.push(args.map((arg) => JSON.stringify(arg, null, 2)).join(" "));
       originalLog.apply(console, args);
     };
 
@@ -35,9 +40,7 @@ export default function Home() {
 
     eval(jsCode);
 
-    const stringifiedLogs = logs.map((log) => JSON.stringify(log));
-
-    setCodeLogs(stringifiedLogs);
+    setCodeLogs(logs);
 
     // Reset back the console log function.
     console.log = originalLog;
@@ -45,13 +48,14 @@ export default function Home() {
 
   return (
     <PanelGroup class="flex h-screen bg-gray-50 text-gray-700">
+      {/* <script src={DAYJS_url} /> */}
       <script src="https://unpkg.com/dayjs@latest/dayjs.min.js" />
       <script src="https://unpkg.com/typescript@latest/lib/typescript.js" />
 
       <Panel id="1" class="w-1/2 bg-gray-50">
         <MonacoEditor
+          path="awesome"
           onChange={(value) => setTSCodeState(value)}
-          // language="typescript"
           value={tsCodeState()}
           language="typescript"
           onMount={(monaco, editor) => {
@@ -81,34 +85,39 @@ export default function Home() {
 
             monaco.editor.setTheme("poimandres");
 
-            // Language Definition extension.
+            // Language Definition extension. https://stackoverflow.com/questions/43058191/how-to-use-addextralib-in-monaco-with-an-external-type-definition
 
             monaco.languages.typescript.typescriptDefaults.setCompilerOptions({
               ...monaco.languages.typescript.typescriptDefaults.getCompilerOptions(),
               allowSyntheticDefaultImports: true,
+              moduleResolution: 1,
+              isolatedModules: true,
+              esModuleInterop: true,
             });
 
             monaco.languages.typescript.typescriptDefaults.addExtraLib(
-              DAYJS_index,
+              DAYJS_index_replaced,
               "file:///node_modules/@types/dayjs/index.d.ts",
             );
 
-            console.log(DAYJS_index);
-
-            monaco.languages.typescript.typescriptDefaults.addExtraLib(
-              "export declare function add(a: number, b: number): number",
-              "file:///node_modules/@types/math/index.d.ts",
-            );
-
+            // For imports on exported dayjs to work:
             const currentModel = editor?.getModel();
 
             const model = monaco.editor.createModel(
               currentModel?.getValue() ?? "",
-              currentModel?.getLanguageId(),
+              currentModel?.getLanguageId() || "typescript",
               monaco.Uri.parse("file:///main.ts"),
             );
 
             editor?.setModel(model);
+
+            setTSCodeState(
+              'const fiveDaysAfter = dayjs().add(5, "days");\n\nconsole.log(fiveDaysAfter.format("MMMM D YYYY"))',
+            );
+
+            setTimeout(() => {
+              executeCode();
+            }, 500);
           }}
           options={{
             minimap: {
@@ -118,48 +127,99 @@ export default function Home() {
         />
       </Panel>
       <ResizeHandle />
-      <Panel id="2" class="flex w-full flex-col bg-gray-50 p-3">
+      <Panel id="2" class="flex w-full flex-col bg-gray-50 p-3" minSize={20}>
+        <div class="py-2">
+          <div class="mb-2 flex items-center gap-x-2">
+            <div class="h-12 w-12 flex-shrink-0 overflow-hidden rounded-md object-contain">
+              <img
+                src="https://day.js.org/img/logo.png"
+                class="h-full w-full"
+                alt="dayjs logo"
+              />
+            </div>
+            <h1 class="text-3xl font-medium">DayJS Studio</h1>
+          </div>
+          <p class="mb-1">
+            A quick TypeScript environment on the browser that uses DayJS so you
+            can calculate dates quickly.
+            <br />
+          </p>
+          <p class="text-sm text-opacity-50">
+            By{" "}
+            <A
+              href="https://carlo.vercel.app/"
+              class="font-medium text-orange-500"
+            >
+              Carlo
+            </A>{" "}
+            💙.
+          </p>
+        </div>
         <div class="py-2">
           <button
             onClick={executeCode}
-            class="rounded-md border border-sky-400 bg-sky-600 px-3 py-2 text-white "
+            class="flex items-center gap-x-1.5 rounded-md border border-orange-400 bg-orange-600 px-3 py-2 pr-5 text-white "
           >
-            Execute Code
+            <IconExecute font-size="22px" />
+            <span>Execute</span>
           </button>
         </div>
 
         <hr />
 
-        <div class="flex flex-col gap-y-2 py-2">
-          <span class="font-medium">Code: </span>
-          <pre class="rounded-md border-gray-500 bg-gray-800 p-2 text-gray-300">
-            {tsCodeState()}
-          </pre>
-        </div>
+        <div class="flex-grow">
+          <Show when={showAdvanced()}>
+            <div class="flex flex-col gap-y-2 py-2">
+              <span class="font-medium">Code: </span>
+              <pre class="rounded-md border-gray-500 bg-gray-800 p-2 text-gray-300">
+                {tsCodeState()}
+              </pre>
+            </div>
 
-        <hr />
+            <hr />
 
-        <div class="flex flex-col gap-y-2 py-2">
-          <span class="font-medium">Transpiled TS to JS: </span>
-          <Show when={!!jsCodeState()} fallback={<div>None</div>}>
-            <pre class="rounded-md border-gray-500 bg-gray-800 p-2 text-gray-300">
-              {jsCodeState()}
-            </pre>
+            <div class="flex flex-col gap-y-2 py-2">
+              <span class="font-medium">Transpiled TS to JS: </span>
+              <Show when={!!jsCodeState()} fallback={<div>None</div>}>
+                <pre class="rounded-md border-gray-500 bg-gray-800 p-2 text-gray-300">
+                  {jsCodeState()}
+                </pre>
+              </Show>
+            </div>
+
+            <hr />
           </Show>
-        </div>
-        <hr />
 
-        <div class="flex flex-col gap-y-2 py-2">
-          <span class="font-medium">Output: </span>
+          <div class="flex flex-col gap-y-2 py-2">
+            <span class="font-medium">Output: </span>
 
-          <Show when={codeLogs()?.length} fallback={<div>None</div>}>
-            <ul class="flex flex-col gap-y-1">
-              <For each={codeLogs()}>
-                {(codeLog, index) => <li>{codeLog}</li>}
-              </For>
-            </ul>
-          </Show>
+            <Show when={codeLogs()?.length} fallback={<div>None</div>}>
+              <ul class="flex min-h-40 flex-col gap-y-1 rounded-md border-gray-500 bg-slate-800 p-2 text-slate-400">
+                <For each={codeLogs()}>
+                  {(codeLog, index) => <li>{codeLog}</li>}
+                </For>
+              </ul>
+            </Show>
+          </div>
         </div>
+
+        <footer>
+          Made with{" "}
+          <A href="https://solidjs.com" class="text-sky-500">
+            SolidJS
+          </A>
+          {", "}
+          <A href="https://day.js.org/" class="font-medium text-orange-500">
+            Day.js
+          </A>
+          {", and "}
+          <A
+            href="https://github.com/microsoft/monaco-editor"
+            class="font-medium text-purple-500"
+          >
+            Monaco
+          </A>
+        </footer>
       </Panel>
     </PanelGroup>
   );
